a371de7719c587bca5998fe6c2fa87a8280187af
Marc Xiao <marc47marc47@gmail.com>
2021-09-23 02:24:30 +0800

    add base gin sample form with tls

---
+++ git-remote-init.sh
@@ -0,0 +1,8 @@
+
+echo "# gin-form" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:marc47marc47/gin-form.git
+git push -u origin main
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+	router.Use(static.Serve("/assets", static.LocalFile("./js", false)))
+	router.Use(static.Serve("/assets", static.LocalFile("./css", false)))
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{})
+	})
+
+	router.GET("/form", func(ctx *gin.Context) {
+		ctx.HTML(200, "form.html", gin.H{})
+	})
+
+	router.GET("/validform", func(ctx *gin.Context) {
+		ctx.HTML(200, "validform.html", gin.H{})
+	})
+
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1658),
+		Subject: pkix.Name{
+			Organization:  []string{"Cobrasonic"},
+			Country:       []string{"Taiwan (R.O.C)"},
+			Province:      []string{"Taipei City"},
+			Locality:      []string{"Daan Dist."},
+			StreetAddress: []string{"3F., No.7, Ln. 116, Guangfu S. Rd."},
+			PostalCode:    []string{"106451"},
+		},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(10, 0, 0),
+		SubjectKeyId: []byte{1, 2, 3, 4, 6},
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature,
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+
+	// Sign the certificate
+	certificate, _ := x509.CreateCertificate(rand.Reader, cert, cert, pub, priv)
+
+	certBytes := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certificate})
+	keyBytes := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+
+	// Generate a key pair from your pem-encoded cert and key ([]byte).
+	x509Cert, _ := tls.X509KeyPair(certBytes, keyBytes)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{x509Cert}}
+	server := http.Server{Addr: ":3000", Handler: router, TLSConfig: tlsConfig}
+
+	log.Println("router: ", router)
+	err := server.ListenAndServeTLS("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//router.Run(":8000")
+}
